feadaf259387deb55dd0166179b06e8f5866ea46
Karthik Krishnamoorthi <karthikk@zivro.com>
2022-07-27 07:55:39 -0400

    commit

---
+++ src/springboot/src/main/java/boot/springboot/controllers/ApiController.java
@@ -0,0 +1,15 @@
+package boot.springboot.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @RequestMapping(path="/" )
+    public String ok() {
+        return "Ok";
+    }
+
+}
